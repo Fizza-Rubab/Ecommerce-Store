b667499d49fdbdf3b48919566fb353730d4e5174
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-17 03:02:37 +0500

    components imported

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello everyone!</h1>
-      </div>
-    );
-  }
+import React from "react";
+import Login from "./components/login";
+// import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import CategoryBar from "./components/categoryBar";
+// import Register from "./components/register";
+function App() {
+  return (
+    // <Button variant="contained" color="primary">
+    //   Hello World
+    // </Button>
+    <CategoryBar />
+    // <Login />
+    // <Register />
+  );
 }
 
 export default App;

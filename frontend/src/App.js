2f25b0e5ce0c469d594854750b82e22fcf472659
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-17 19:32:34 +0500

    components imported for ui testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Login from "./components/login";
-// import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
 import CategoryBar from "./components/categoryBar";
-// import Register from "./components/register";
+import Register from "./components/register";
+import AppBar from "./components/appbar";
 function App() {
   return (
     // <Button variant="contained" color="primary">
     //   Hello World
     // </Button>
-    <CategoryBar />
+    //<CategoryBar />
+    <AppBar />
     // <Login />
     // <Register />
   );

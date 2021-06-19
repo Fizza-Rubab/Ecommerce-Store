ec4a703e734fa24356a6fd00078d3512221d1e1a
hsntrq <mohdhasantariq@gmail.com>
2021-06-19 20:43:23 +0500

    added category model and address

---
+++ models/address.model.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+const db = require("../config/db");
+
+const Address = db.define(
+  "address",
+  {
+    default: {
+      type: Sequelize.BOOL,
+      defaultValue: false,
+    },
+    streetAddress: {
+      type: Sequelize.STRING(32),
+      allowNull: false,
+    },
+    zip: {
+      type: Sequelize.STRING(5),
+      allowNull: false,
+    },
+    city: {
+      type: Sequelize.STRING(20),
+      allowNull: false,
+    },
+    country: {
+      type: Sequelize.STRING(20),
+    },
+  },
+  {}
+);
+
+Address.sync({ alter: true }).then(() => console.log("User table connected"));
+
+module.exports = Address;

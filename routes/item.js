b4b79fb292131aa6393a3407cd6c0e3eeb4dfcf3
Aqibkhan2023 <63522761+Aqibkhan2023@users.noreply.github.com>
2021-06-20 02:33:21 +0500

    Controller for items created

--- routes/item.js
+++ routes/item.js
@@ -1,26 +1,47 @@
 const router = require("express").Router();
-const Item = require("../models/item.model");
+// let Item = require("../models/item.model");
+const { catchErrors } = require("../errorHandlers");
 
-router.route("/").get((req, res) => {
-  Item.findAll()
-    .then((items) => res.json(items))
-    .catch((err) => res.status(400).json({ Error: err }));
-});
+const {
+  findAllItems,
+  findItem,
+  addItem,   
+} = require("../controllers/itemController");
 
-router.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  Item.findByPk(id)
-    .then((item) => res.json(item))
-    .catch((err) => res.status(404).json({ Error: err }));
-});
+router.get("/", catchErrors(findAllItems));
 
-router.route("/add").post((req, res) => {
-  const { name, quantity, price, description } = req.body;
-  const newItem = new Item({ name, quantity, price, description });
-  newItem
-    .save()
-    .then(() => res.json({ item: newItem }))
-    .catch((err) => res.status(400).json({ Error: err }));
-});
+router.get("/:id", catchErrors(findItem));
+
+router.post("/add", catchErrors(addItem));
+
 
 module.exports = router;
+
+// router.route("/").get((req, res) => {
+//   Item.findAll()
+//     .then((items) => res.json(items))
+//     .catch((err) => res.status(400).json({ Error: err }));
+// });
+
+// router.route("/:id").get((req, res) => {
+//     const id = req.params.id;
+//     Item.findByPk(id)
+//       .then((item) => res.json(item))
+//       .catch((err) => res.status(404).json({ Error: err }));
+//   });
+
+// router.route("/add").post((req, res) => {
+//   const {name, quantity, price, description} = req.body;
+//   const newItem = new Item({
+//     name,
+//     quantity,
+//     price,
+//     description,
+//   });
+//   newItem
+//     .save()
+//     .then(() => res.json({ item: newItem }))
+//     .catch((err) => res.status(400).json({ Error: err }));
+// });
+
+// module.exports = router;

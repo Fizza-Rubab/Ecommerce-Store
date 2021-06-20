08c558b92bd6246424dcaa95ee0939716a1f70af
Aqibkhan2023 <63522761+Aqibkhan2023@users.noreply.github.com>
2021-06-21 01:48:16 +0500

    image Handlers added to item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,27 +1,46 @@
+const { map } = require("bluebird");
 const { Item, Category } = require("../models");
 
 exports.findAllItems = async (req, res) => {
-  await Item.findAll({include: [Category]})
+  const items = await Item.findAll({ include: [Category] });
+  items.map((item) => {
+    item["image"] = Image.findOne({ where: { item_id: item.id } });
+  });
+};
+
+exports.findAllItemsCategory = async (req, res) => {
+  await Item.findAll({ where: { category_id: id }, include: [Category] })
     .then((items) => res.json(items))
     .catch((err) => res.status(400).json({ Error: err }));
 };
 
-exports.findAllItemsCategory = async (req, res) => {
-  await Item.findAll({where: {category_id:id}, include: [Category]})
-    .then((items) => res.json(items))
-    .catch((err) => res.status(400).json({ Error: err }));
+exports.addImage = async (req, res) => {
+  const { imageType, imageName, imageData } = await req.body;
+  const image = new Image({ imageType, imageName, imageData, item_id: id });
+  image.save();
 };
 
 exports.findItem = async (req, res) => {
   const id = await req.params.id;
-  Item.findByPk(id)
-    .then((item) => res.json(item))
-    .catch((err) => res.status(404).json({ Error: err }));
+
+  let item = await Item.findByPk(id);
+
+  item["image"] = Image.findAll({ where: { item_id: id } });
+
+  res.json(item);
 };
 
 exports.addItem = async (req, res) => {
-  const { name, quantity, price, description, seller_id, category_id } = req.body;
-  const newItem = new Item({ name, quantity, price, description, seller_id, category_id });
+  const { name, quantity, price, description, seller_id, category_id } =
+    req.body;
+  const newItem = new Item({
+    name,
+    quantity,
+    price,
+    description,
+    seller_id,
+    category_id,
+  });
   newItem
     .save()
     .then(() => res.json({ item: newItem }))
--- routes/item.js
+++ routes/item.js
@@ -6,11 +6,14 @@ const {
   findItem,
   addItem,   
   findAllItemsCategory,
+  addImage,
 } = require("../controllers/itemController");
 
 router.get("/", catchErrors(findAllItems));
 
 router.get("/category/:id", catchErrors(findAllItemsCategory));
+
+router.get("/addImage/:id", catchErrors(addImage));
 
 router.get("/:id", catchErrors(findItem));
 

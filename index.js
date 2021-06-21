b1a24d3553caad4ff1a5a6fc226a5cd3aa69bb0c
hsntrq <mohdhasantariq@gmail.com>
2021-06-21 10:10:48 +0500

    db authentication refactored

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
 const upload = require("multer")();
+require("dotenv").config();
+require("./models");
 
 const app = express();
-const port = process.env.PORT || 5000;
-app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.use(require("cors")());
 app.use(express.json());
 
-const db = require("./config/db");
-
-db.authenticate()
-  .then(() => {
-    console.log(
-      "Connection with postgresql has been established successfully."
-    );
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-
 const itemsRouter = require("./routes/item");
-
 app.use("/api/item", itemsRouter(upload));
 
 const usersRouter = require("./routes/user");
-
 app.use("/api/users", usersRouter(upload));
 
 const cartRouter = require("./routes/cart");
-
 app.use("/api/cart", cartRouter);
 
 const categoryRouter = require("./routes/category");
-
 app.use("/api/categories", categoryRouter);
 
 const checkoutRouter = require("./routes/checkout");
-
 app.use("/api/checkout", checkoutRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });

8f771ff5908ef7fad61b7d0554fa7d9b10360d5a
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-22 03:59:16 +0500

    extra things removed and optimised

--- frontend/src/components/userProfile.js
+++ frontend/src/components/userProfile.js
@@ -3,19 +3,12 @@ import axios from "axios";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 import { useHistory } from "react-router-dom";
 
 const StyledTableCell = withStyles((theme) => ({
@@ -35,18 +28,6 @@ const StyledTableRow = withStyles((theme) => ({
     },
   },
 }))(TableRow);
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -82,17 +63,22 @@ export default function UpdateProfile(user) {
   const [userName, setUserName] = useState("");
   const token = window.localStorage.getItem("E_Token");
   const id = `${JSON.parse(atob(token.split(".")[1])).user_id}`;
-
   useEffect(() => {
     if (!token) history.push("/login");
-    axios
-      .get(`http://localhost:5000/api/users/${id}`)
+    axios({
+      method: "get", //you can set what request you want to be
+      url: `http://localhost:5000/api/users/${id}`,
+      headers: {
+        authorization: token,
+      },
+    })
       .then((res) => {
         setUserName(res.data.userName);
       })
-      .catch(function (error) {
-        console.log(error);
+      .catch((err) => {
+        console.log(err);
       });
+
     //eslint-disable-next-line
   }, []);
   return (
@@ -108,12 +94,14 @@ export default function UpdateProfile(user) {
             className={classes.form}
             onSubmit={(e) => {
               e.preventDefault();
-              axios
-                .put(`http://localhost:5000/api/users/update/${id}`, {
-                  userName: userName,
-                })
+              axios({
+                method: "put", //you can set what request you want to be
+                url: `http://localhost:5000/api/users/update/${id}`,
+                headers: {
+                  authorization: token,
+                },
+              })
                 .then((res) => {
-                  localStorage.setItem("E_Token", res.data.token);
                   history.push("/home");
                 })
                 .catch((error) => {

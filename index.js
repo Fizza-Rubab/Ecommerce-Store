f8596909c274a3be1b04845fc5304b7a67409794
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-23 00:42:07 +0500

    deployment

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const upload = require("multer")();
+const path = require("path");
+
 require("dotenv").config();
 require("./models");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+}
 
 app.use(require("cors")());
 app.use(express.json());
@@ -24,6 +30,10 @@ app.use("/api/categories", categoryRouter);
 
 const checkoutRouter = require("./routes/checkout");
 app.use("/api/checkout", checkoutRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);

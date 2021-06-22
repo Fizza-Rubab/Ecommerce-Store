a409091bfd12ce84909ce1f496586479a0933b33
Aqibkhan2023 <63522761+Aqibkhan2023@users.noreply.github.com>
2021-06-22 21:37:12 +0500

    index.js reduntant code removed

--- index.js
+++ index.js
@@ -2,27 +2,10 @@ const express = require("express");
 const upload = require("multer")();
 require("dotenv").config();
 require("./models");
-const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-const devConfig = {
-  user: "postgres",
-  password: "Abcde123",
-  host: "localhost",
-  database: "pernstack",
-  port: 5432,
-}
-
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-if (process.env.NODE_ENV === "production") {
-  //server static content
-  //npm run build
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-}
 
 app.use(require("cors")());
 app.use(express.json());
